5f3c254d4abe296ac8bc8cd356cc37c69c0b5f9a
michaelclancy90 <michaelclancy@hotmail.co.uk>
2022-10-11 20:08:07 +1100

    auth file fixed for gql

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,17 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+    mutation addUser($)
+`;
+
+export const LOGIN_USER = gql`
+    mutation loginUser($)
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($)
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($)
+`;
