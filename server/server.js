b3c8e3004de2fa6ea4e7b92ff8a57a531e4863e8
michaelclancy90 <michaelclancy@hotmail.co.uk>
2022-10-14 23:05:45 +1100

    pushing to heroku

--- server/server.js
+++ server/server.js
@@ -18,14 +18,9 @@ app.use(express.json());
 
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-/* app.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-}); */
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/'));
