ec3e6055caa9fa99fb42de502542108db131f33d
michaelclancy90 <michaelclancy@hotmail.co.uk>
2022-10-14 19:37:34 +1100

    mutation file updated. code debug

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,17 +1,50 @@
 import { gql } from '@apollo/client';
 
 export const ADD_USER = gql`
-    mutation addUser($)
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
 `;
 
 export const LOGIN_USER = gql`
-    mutation loginUser($)
+  mutation loginUser($email: String!, $password: String!) {
+    login(username: $email, password: $password) {
+      token
+      user {
+        username
+      }
+    }
+  }
 `;
 
 export const SAVE_BOOK = gql`
-    mutation saveBook($)
+    mutation saveBook($input: bookInput!){
+        savebook(input: $input!) {           
+            savedbooks{
+                authors
+                description
+                bookId
+                image
+                link
+                title             
+                
+        }
+    }
+}
 `;
 
 export const REMOVE_BOOK = gql`
-    mutation removeBook($)
+    mutation removeBook($bookId: ID!!){
+        removeBook(bookId: $bookId!) {           
+            savedbooks{
+                bookId
+        }
+    }
+    }
 `;
